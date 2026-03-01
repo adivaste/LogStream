789103dbe7c970cf5728326b82d2d3bd292a39f1
adivaste <adivaste@outlook.com>
2026-03-01 15:50:09 +0530

    chore(initial-project-setup): initialize Chrome extension UI scaffold with Vite, React, and Tailwind

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# LogStream
 Chrome extension which allows to view salesforce/apex log hustle free with better UI and accessibility.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className='font-sans text-2xl font-bold'>LogStream</h1>
+        </div>
+    )
+}
+
+export default App
